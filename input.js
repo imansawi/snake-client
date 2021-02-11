21bb177e6c50cb3ea8324e1a1cd71b2ed6c5e694
imansawi <imansawi@hotmail.com>
2021-02-11 00:51:21 +0000

    created WASD movement

--- input.js
+++ input.js
@@ -2,7 +2,11 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -18,6 +22,30 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+
+  if (data === 'w') {
+    connection.write("Move: up");
+  }
+  if (data === 'a') {
+    connection.write("Move: left");
+  }
+  if (data === 's') {
+    connection.write("Move: down");
+  }
+  if (data === 'd') {
+    connection.write("Move: right");
+  }
+
+  if (data === '1') {
+    connection.write("Say: I am a WINNER!");
+  }
+  if (data === '2') {
+    connection.write("Say: L O S E R!");
+  }
+  if (data === '3') {
+    connection.write("Say: Good Game!");
+  }
+  
 };
 
 module.exports = { setupInput };
